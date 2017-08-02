47258e4a818e855a09a7aa9232e59ae9e4efbacd
javaman2017 <tommooney2017@outlook.com>
2017-08-02 17:13:32 +0100

    Version 1 of c-samples

---
+++ c-samples/firstprog.c
@@ -0,0 +1,19 @@
+/*
+ * firstprog.c
+ *
+ *  Created on: 2 Aug 2017
+ *      Author: user
+ */
+
+
+#include <stdio.h>
+
+int main(){
+
+	int i;
+
+	for(i = 0;  i < 10; i++)
+	   printf("Hello, world!\n");
+	return 0;
+}
+
