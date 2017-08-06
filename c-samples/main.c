420280284bfe706a3b4aa6127bb39c557e4f8f67
javaman2017 <tommooney2017@outlook.com>
2017-08-06 13:18:23 +0100

    Added header to table. Added method to calculate and display the reverse table

--- c-samples/main.c
+++ c-samples/main.c
@@ -18,6 +18,7 @@ void fahr_to_celsius_table(){
 	int upper = 300; 	/* upper limit */
 	int step = 20;
 
+	printf("Fahr\tCel\n");
 	fahr = lower;
 	while(fahr <= upper){
 		celsius = 5 * (fahr - 32) / 9;
@@ -26,8 +27,29 @@ void fahr_to_celsius_table(){
 	}
 }
 
+/* print Celsius-Fahrenheit table
+ * for cel = -17, -6, ..., 148
+ */
+void celsius_to_fahr_table(){
+	int celsius, fahr;
+
+	int lower = -17;		/* lower limit of temperature table */
+	int upper = 148; 	/* upper limit */
+	int step = 11;
+
+	printf("Cel\tFahr\n");
+	celsius = lower;
+	while(celsius <= upper){
+		fahr = ((9 * celsius) / 5) + 32;
+		printf("%d\t%d\n", celsius,fahr);
+		celsius = celsius + step;
+	}
+}
+
 int main(){
 	fahr_to_celsius_table();
+	printf("\n\n");
+	celsius_to_fahr_table();
 }
 
 
